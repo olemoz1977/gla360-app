cd8d36367b14a35fd2c7bd1dd24a62c6ab36aa30
olemoz1977 <olemoz1977@gmail.com>
2025-10-06 14:43:52 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import ReviewLink from "./pages/ReviewLink";
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/review/:id" element={<ReviewLink />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
