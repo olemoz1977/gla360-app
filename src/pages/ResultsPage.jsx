ba3fa2a96abd2c640b6f7ab3cf3e82ed7e79cd80
olemoz1977 <olemoz1977@gmail.com>
2025-10-07 03:19:01 +0300

    Update ResultsPage.jsx

--- src/pages/ResultsPage.jsx
+++ src/pages/ResultsPage.jsx
@@ -1,1 +1,11 @@
 
+export default function ResultsPage() {
+  return (
+    <div>
+      <h1>Results</h1>
+      <p>Čia vėliau parodysime grafikus ir palyginimus (self vs raters).</p>
+    </div>
+  )
+}
+
+
