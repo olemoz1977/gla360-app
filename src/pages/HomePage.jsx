73512f827d36755e0b9c3e1c038122c3bc2e0d5a
olemoz1977 <olemoz1977@gmail.com>
2026-08-18 22:34:56 +0300

    Rename app home branding to Leadership 360

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <section>
-      <h1>GLA360</h1>
+      <h1>Leadership 360°</h1>
       <p>Sveikas sugrįžęs, Oleg. Pradėk nuo Self Assessment 🙂</p>
     </section>
   )
